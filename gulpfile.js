5664940552d7bd340b5f3895a1dd907d4d8d8a57
mhubenthal <hubentmx@gmail.com>
2015-04-02 16:46:16 -0400

    update config

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,25 @@ var gulp = require('gulp'),
 
 gulp.task('js',function(){
   gulp.src(['source/js/gol/gol.js','source/js/gol-2/gol-2.js','source/js/ticker/ticker.js','source/js/main.js'])
-  //	.pipe(plugins.watch('source/js/*.js')) 
 		.pipe(plugins.jshint())
 		.pipe(plugins.jshint.reporter('default'))
 		.pipe(plugins.concat('app.js'))
 	  .pipe(plugins.uglify())
 		.pipe(gulp.dest('build/js'))
-	//	.pipe(plugins.connect.reload());
+		.pipe(plugins.livereload());
 });
 
-gulp.task('css',function(){
+gulp.task('less',function(){
   gulp.src('source/less/main.less')
-    .pipe(plugins.watch('source/less/main.less'))
     .pipe(plugins.less())
     .pipe(gulp.dest('build/css'))
-		.pipe(plugins.connect.reload());
+		.pipe(plugins.livereload());
 });
 
 gulp.task('html',function(){
 	gulp.src('source/*.html')
-		.pipe(plugins.watch('source/*.html'))
 		.pipe(gulp.dest('build'))
-		.pipe(plugins.connect.reload());
+		.pipe(plugins.livereload());
 });
 
 gulp.task('connect',function(){
@@ -36,4 +33,11 @@ gulp.task('connect',function(){
 	});
 });
 
-gulp.task('default',['connect','css','js','html']); 
+gulp.task('watch',function(){
+	plugins.livereload.listen();
+	gulp.watch('source/less/main.less');
+	gulp.watch('source/*.html');
+	gulp.watch(['source/js/**/*.js','source/js/main.js']);	
+})
+
+gulp.task('default',['connect','less','js','html','watch']); 
